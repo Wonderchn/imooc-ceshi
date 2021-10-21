101f69eee9b9e0d90db3bafa09cbf0b77ebe8a9e
小明 <a1073506500@qq.com>
2021-10-21 23:44:11 +0800

    service层实现相关代码修改

--- foodie-dev-service/src/main/java/com/imooc/service/impl/StuServiceImpl.java
+++ foodie-dev-service/src/main/java/com/imooc/service/impl/StuServiceImpl.java
@@ -20,18 +20,30 @@ public class StuServiceImpl implements StuService {
         return stuMapper.selectByPrimaryKey(id);
     }
 
+
+    @Transactional(propagation = Propagation.REQUIRED)
     @Override
-    public void saveStu() {
+    public void saveStu(){
+        Stu stu = new Stu();
+        stu.setName("jack");
+        stu.setAge(19);
+        stuMapper.insert(stu);
 
     }
 
+    @Transactional(propagation = Propagation.REQUIRED)
     @Override
-    public void updateStu() {
-
+    public void updateStu(int id) {
+        Stu stu = new Stu();
+        stu.setId(id);
+        stu.setName("lucy");
+        stu.setAge(20);
+        stuMapper.updateByPrimaryKey(stu);
     }
 
+    @Transactional(propagation = Propagation.REQUIRED)
     @Override
-    public void deleteStu() {
-
+    public void deleteStu(int id) {
+        stuMapper.deleteByPrimaryKey(id);
     }
 }
