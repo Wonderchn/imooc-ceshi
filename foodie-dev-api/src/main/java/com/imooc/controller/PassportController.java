fef2e21e0b69fbdce33b7903d272ec4ee64ed363
小明 <a1073506500@qq.com>
2021-10-26 16:05:19 +0800

    用户注册功能初上线

--- foodie-dev-api/src/main/java/com/imooc/controller/PassportController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/PassportController.java
@@ -1,16 +1,13 @@
 package com.imooc.controller;
 
 
+import com.imooc.pojo.bo.UserBO;
 import com.imooc.service.UserService;
 import com.imooc.utils.IMOOCJSONResult;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("passport")
@@ -38,5 +35,42 @@ public class PassportController {
         return IMOOCJSONResult.ok();
     }
 
+
+
+    @PostMapping("/regist")
+    public IMOOCJSONResult usernameIsExist(@RequestBody UserBO userBo){
+        String username = userBo.getUsername();
+        String password = userBo.getPassword();
+        String confirmPassword = userBo.getConfirmPassword();
+
+        // 0.判断用户名和密码必须不为空
+        if (StringUtils.isBlank(username) ||
+            StringUtils.isBlank(password) ||
+            StringUtils.isBlank(confirmPassword)){
+            return IMOOCJSONResult.errorMsg("用户名或密码不能为空");
+        }
+
+        // 1.查询用户名是否存在
+        boolean isExist = userService.queryUsernameIsExist(username);
+        if (isExist){
+            return  IMOOCJSONResult.errorMsg("用户名已经存在");
+        }
+
+        // 2.密码长度不能少于6位
+        if (password.length() <6){
+            return IMOOCJSONResult.errorMsg("密码的长度不能少于6");
+        }
+
+        // 3.判断两次密码是否一致
+        if (!password.equals(confirmPassword)){
+            return IMOOCJSONResult.errorMsg("密码不一致");
+        }
+
+        // 4.实现注册
+        userService.createUser(userBo);
+
+        //请求成功
+        return IMOOCJSONResult.ok();
+    }
 
 }
