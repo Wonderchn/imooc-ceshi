e25b887dea7c83395076711407c61478053f2709
小明 <a1073506500@qq.com>
2021-10-28 14:50:36 +0800

    轮播图相关功能上线

---
+++ foodie-dev-service/src/main/java/com/imooc/service/CarouselService.java
@@ -0,0 +1,18 @@
+package com.imooc.service;
+
+import com.imooc.pojo.Carousel;
+
+import java.util.List;
+
+/**
+ * @author chn
+ */
+public interface CarouselService {
+    /**
+     * 查询所有轮播图列表
+     * @param isShow
+     * @return
+     */
+    public List<Carousel> queryAll(Integer isShow);
+
+}
