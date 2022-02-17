ed58c6e94e234ccb72d88760b11583f37b1c395f
Wonderchn <a1073506500@QQ.COM>
2022-02-17 11:18:48 +0800

    2022年2月17日 CAS单点登录功能实现

--- foodie-dev-common/src/main/java/com/imooc/utils/IMOOCJSONResult.java
+++ foodie-dev-common/src/main/java/com/imooc/utils/IMOOCJSONResult.java
@@ -1,26 +1,26 @@
 package com.imooc.utils;
 
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
- *
+ * 
  * @Title: IMOOCJSONResult.java
  * @Package com.imooc.utils
  * @Description: 自定义响应数据结构
  * 				本类可提供给 H5/ios/安卓/公众号/小程序 使用
  * 				前端接受此类数据（json object)后，可自行根据业务去实现相关功能
- *
+ * 
  * 				200：表示成功
  * 				500：表示错误，错误信息在msg字段中
  * 				501：bean验证错误，不管多少个错误都以map形式返回
  * 				502：拦截器拦截到用户token出错
  * 				555：异常抛出信息
  * 				556: 用户qq校验异常
+ * 			    557: 校验用户是否在CAS登录，用户门票的校验
  * @Copyright: Copyright (c) 2020
  * @Company: www.imooc.com
- * @author 慕课网 - 风间影月
+ * @author
  * @version V1.0
  */
 public class IMOOCJSONResult {
@@ -36,7 +36,7 @@ public class IMOOCJSONResult {
 
     // 响应中的数据
     private Object data;
-
+    
     @JsonIgnore
     private String ok;	// 不使用
 
@@ -47,7 +47,7 @@ public class IMOOCJSONResult {
     public static IMOOCJSONResult build(Integer status, String msg, Object data, String ok) {
         return new IMOOCJSONResult(status, msg, data, ok);
     }
-
+    
     public static IMOOCJSONResult ok(Object data) {
         return new IMOOCJSONResult(data);
     }
@@ -55,23 +55,27 @@ public class IMOOCJSONResult {
     public static IMOOCJSONResult ok() {
         return new IMOOCJSONResult(null);
     }
-
+    
     public static IMOOCJSONResult errorMsg(String msg) {
         return new IMOOCJSONResult(500, msg, null);
     }
 
+    public static IMOOCJSONResult errorUserTicket(String msg) {
+        return new IMOOCJSONResult(557, msg, null);
+    }
+    
     public static IMOOCJSONResult errorMap(Object data) {
         return new IMOOCJSONResult(501, "error", data);
     }
-
+    
     public static IMOOCJSONResult errorTokenMsg(String msg) {
         return new IMOOCJSONResult(502, msg, null);
     }
-
+    
     public static IMOOCJSONResult errorException(String msg) {
         return new IMOOCJSONResult(555, msg, null);
     }
-
+    
     public static IMOOCJSONResult errorUserQQ(String msg) {
         return new IMOOCJSONResult(556, msg, null);
     }
@@ -85,7 +89,7 @@ public class IMOOCJSONResult {
         this.msg = msg;
         this.data = data;
     }
-
+    
     public IMOOCJSONResult(Integer status, String msg, Object data, String ok) {
         this.status = status;
         this.msg = msg;
@@ -127,12 +131,12 @@ public class IMOOCJSONResult {
         this.data = data;
     }
 
-    public String getOk() {
-        return ok;
-    }
+	public String getOk() {
+		return ok;
+	}
 
-    public void setOk(String ok) {
-        this.ok = ok;
-    }
+	public void setOk(String ok) {
+		this.ok = ok;
+	}
 
 }
